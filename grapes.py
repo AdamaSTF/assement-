b4ff9be98d846dc764507e78bd7728c6f9eed7c0
AdamaSTF <adamasene1.tall@gmail.com>
2024-11-08 12:27:41 +0000

    Create grapes.py

---
+++ grapes.py
@@ -0,0 +1,15 @@
+student_mark= input("enter the student grades ")
+if 70 <= student_mark <= 100:
+                    print ("your grade is A")
+elif 60 <= student_mark <= 69:
+                    print ("your grade is B")
+elif 50 <= student_mark <= 59:
+                    print ("your grade is C")
+elif 40 <= student_mark <= 49:
+                    print ("your grade is D")
+elif 30 <= student_mark <= 39:
+                    print ("your grade is E")
+elif 20 <= student_mark <= 29:
+                    print ("your grade is F")
+else:
+  print ("invalived mark")
