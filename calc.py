8e9f083d78424d941a60705b2cb2161f92f1c5c7
AdamaSTF <adamasene1.tall@gmail.com>
2024-11-08 11:58:10 +0000

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,12 @@
+#asking the user to enter to number 
+num1= float(input("enter number 1 " ))
+num2= float(input ( "enetr numner 2 "))
+#now we are calcularting 
+add_result= num1 + num2
+diff = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+print ("sum: ", add_result)
+print ("diff: ", diff)
+print ("product: ",product)
+print ("quotient: ",quotient)
